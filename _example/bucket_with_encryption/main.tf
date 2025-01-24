3080c72401c79799e2c12da12e2bee5495560444
ashishyadav83 <ashish@syncarc.net>
2025-01-24 16:00:03 +0530

    initial commit

---
+++ _example/bucket_with_encryption/main.tf
@@ -0,0 +1,45 @@
+provider "google" {
+  project = "soy-smile-435017-c5"
+  region  = "asia-northeast1"
+  zone    = "asia-northeast1-a"
+}
+
+module "kms_key" {
+  source          = "git::https://github.com/SyncArcs/terraform-google-kms.git?ref=v1.0.0"
+  name            = "SyncArcs"
+  environment     = "test"
+  location        = "us-central1"
+  prevent_destroy = true
+  keys            = ["test"]
+  role            = ["roles/owner"]
+}
+
+# Bucket module with encryption using the KMS key
+module "bucket" {
+  source      = "./../../"
+  name        = "bucket-encryption"
+  environment = "test"
+  location    = "us-central1" # Use a regional bucket to match the KMS region
+  encryption = {
+    kms_key = module.kms_key.key_id
+  }
+
+  lifecycle_rules = [{
+    action = {
+      type = "Delete"
+    }
+    condition = {
+      age            = 365
+      with_state     = "ANY"
+      matches_prefix = "test12"
+    }
+  }]
+
+  iam_members = [
+    {
+      role   = "roles/storage.admin"
+      member = "group:test-gcp-ops@test.blueprints.joonix.net"
+    }
+  ]
+  autoclass = true
+}
